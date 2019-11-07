5b6db358395180a6155934af629db1066e627a2e
Code <code@COCTL-41256.local>
2019-11-07 15:13:51 -0800

    3:12

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let url = "data.json";
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         let myArr = JSON.parse(this.responseText);
-        triviaQuestions(myArr);
+        tQuestions(myArr);
     }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function triviaQuestions(info){
+function tQuestions(info){
     console.log(info);
 }

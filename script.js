e2b185fc0d9dae4bac2aa648b35776eb3c96de8b
Code <code@COCTL-41256.local>
2019-11-07 14:02:05 -0800

    2:00pm com

---
+++ script.js
@@ -0,0 +1,11 @@
+let xmlhttp = new XMLHttpRequest();
+let url = "js.json";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();

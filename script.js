80e03b4721347812d1e5a1ed962d99e9712397c8
Code <code@COCTL-41256.local>
2019-11-08 10:25:26 -0800

    JSon info added

--- script.js
+++ script.js
@@ -10,6 +10,42 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
+//create and array
+let hQ = [];
+let hQObj = [];
+
+
+
+
 function tQuestions(info){
     console.log(info);
+
+    for (let i = 0; i< info.hQ.length; i++){
+        //Create and Oject for esch loop in array
+         let nFO = {
+             "q":info.hQ[i].q,
+             "aA":info.hQ[i].aA
+         };
+
+
+
+        //console.log(info.hQ[i].q);
+        //console.log(info.hQ[i].aA);
+        hQ.push(info.hQ[i].q);
+        hQ.push(info.hQ[i].aA);
+
+        console.log("Format Arr"+ hQ);
+
+        hQObj.push(nFO);
+        console.log("Trivia Question and First answer OBJECT ARRAY"+ hQObj[i].hQ);
+
+
+
+
+
+        //this is using the Object
+        nFO.q = info.hQ[i].q;   
+
+
+    }
 }

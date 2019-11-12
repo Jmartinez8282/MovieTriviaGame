278b29adb183052ac021303cf589e5fb47dc6ede
Code <code@COCTL-41256.local>
2019-11-12 12:21:09 -0800

    cp

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
 
 //-----------DECLARE OUR VARIABLES
-//let diff = "easy";
+let diff = "easy";
 let triviaQ = [];
 let totalQuestions = 20;
 let totalScore = 0;
 let Incorrect = 0;
 
 let tQuestions = [];
-let diff='easyQ.json';
+let diff1='easyQ.json';
 let qNum = 0;
 
+/////Randmomize 150 questions and selectt 20 questions///---------------------------------///
+
+function loadQuestions(){
+    let xmlhttp = new XMLHttpRequest();
+    let url = "";
+
+    if(diff== "easy"){
+        url="easyQ.json";
+    }
+    
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let myArr = JSON.parse(this.responseText);
+            allQuestions(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+function allQuestions(q){
+    console.log(q.ezQ[49]);
+    let qNum = 0;
+    for (let i = 0; i < totalQuestions; i++)
+    {
+      //  We are going to shuffle
+        qNum = Math.floor(Math.random() * q.ezQ.length);
+        console.log(qNum);
+        triviaQ.push(q.ezQ[qNum]);
+        q.ezQ.splice(qNum,1);
+        
+
+    }
+    console.log(triviaQ);
+}
+
+loadQuestions();    
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------////
 /////GRAB ALL OUR ELMENTS FROM HTML PAGE//
 //CORRECT, COUNTER, QUESTIONS, BUTTONS-USING A CLASS
 
@@ -22,7 +63,7 @@ let a2 = document.getElementById('a2');
 let a3 = document.getElementById('a3');
 let a4 = document.getElementById('a4');
 
-//// get our butetons and add eventlisteners---------//
+//// get our buttons and add eventlisteners---------//
 let buttons = document.getElementsByClassName('btn')
 
 
@@ -88,42 +129,8 @@ function checkAnswer(answer){
 
 
 ///---------------------//-------------------------------///
-loadJSON(diff)
+loadJSON(diff1)
+////----------------------------------------------------------------------------------------------------------------------------///////
 
-
-//function loadQuestions(){
-  //  let xmlhttp = new XMLHttpRequest();
-    //let url = "";
-
-   // if(diff== "easy"){
-      //  url="easyQ.json";
-    //}
-    
-    //xmlhttp.onreadystatechange = function() {
-      //  if (this.readyState == 4 && this.status == 200) {
-        //    let myArr = JSON.parse(this.responseText);
-          //  allQuestions(myArr);
-        //}
-    //};
-    //xmlhttp.open("GET", url, true);
-    //xmlhttp.send();
-//}
-//function allQuestions(q){
-  //  console.log(q.ezQ[26]);
-    //let qNum = 0;
-    //for (let i = 0; i < totalQuestions; i++)
-    //{
-        //We are going to shuffle
-      //  qNum = Math.floor(Math.random() * q.ezQ.length);
-        //console.log(qNum);
-        //triviaQ.push(q.ezQ[qNum]);
-        //q.ezQ.splice(qNum,1);
-        
-
-    //}
-    //console.log(triviaQ);
-//}
-//
-//loadQuestions();    
 
 

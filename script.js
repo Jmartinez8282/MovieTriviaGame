10d6680eb81ae91d91af1e0e34a5ed54084ae422
Code <code@COCTL-41256.local>
2019-11-14 15:23:31 -0800

    cp

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
 //-----------DECLARE OUR VARIABLES
 let inject = document.getElementById('inject');
-
-let diff = "easy";  //Can be medium or hard
-let triviaQ = [];
-let totalQuestions = 20;
-let totalScore = 0;
-let Incorrect = 0;
-let timer = 5;
-let interval;
 let tQuestions = [];
-let diff1 = 'easyQ.json';
-let qNum = 0;
 
+function loadJSON(url) {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            tQuestions = JSON.parse(this.responseText).easyQ;
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
 
-
+loadJSON('easyQ.json')
 //---------Trying to Inject pages---------------//////
 
 function loadHTML(url) {
@@ -24,14 +24,14 @@ function loadHTML(url) {
             let myArr = this.responseText;
             console.log(url);
             if (url === 'gamePage.html') {
-                loadGamePage(myArr);
+                loadGamePage(myArr, tQuestions);
             } else if (url === 'options.html') {
                 loadOptionsPage(myArr);
             } else if (url === 'menu.html') {
                 loadMenuPage(myArr);
             } else if (url === 'instruction.html') {
                 loadInstrucionPage(myArr);
-            } else if ( url === 'title.html') {
+            } else if (url === 'title.html') {
                 loadTitle(myArr);
             } else {
                 alert("loadHTML if statement dude");
@@ -51,22 +51,105 @@ function loadTitle(info) {
         loadHTML('gamePage.html');
     })
 
-    optionBtn.addEventListener('click', function(e){
+    optionBtn.addEventListener('click', function (e) {
         loadHTML('options.html');
     })
 }
 
-function loadGamePage(info) {
+function loadGamePage(info, arr) {
     inject.innerHTML = info;
+    let triviaQ = [];
+    let totalQuestions = 20;
+    let totalScore = 0;
+    let Incorrect = 0;
+    let timer = 5;
+    let interval;
+    let qNum = 0;
     let correct = document.getElementById('C');
     let counter = document.getElementById('counter');
     let questions = document.getElementById('questions');
-
     let a1 = document.getElementById('a1');
     let a2 = document.getElementById('a2');
     let a3 = document.getElementById('a3');
     let a4 = document.getElementById('a4');
 
+    allQuestions(arr);
+    loadQuestion();
+///------add eventlisteners-----/////
+
+
+    function loadQuestion() {
+        questions.innerText = triviaQ[qNum].q;
+
+        a1.innerText = triviaQ[qNum].a1;
+        a2.innerText = triviaQ[qNum].a2;
+        a3.innerText = triviaQ[qNum].a3;
+        a4.innerText = triviaQ[qNum].a4;
+
+        //for loop in buttons//
+    }
+    ///-----------------//////
+    function checkAnswer(answer) {
+        //Retrive the answer and see if its correct
+        //increment your correct number
+
+        if (answer === triviaQ[qNum].C) {
+            totalScore++;
+        }
+        else {
+            incorrect++;
+        }
+        C.innerText = '${totalScore}/${totalQuestions}';
+        timer = 5;
+        counter.innerText = timer;
+        nextQuestion();
+    }
+
+    ///next question//
+
+    function nextQuestions() {
+        //prep  to go to the next question
+        //loadQuestion
+        if (qNum < totalQuesitons) {
+            ///will runutil you hit toal questions = 20;
+            qNum++;
+            loadQuestion();
+        }
+        else {
+            //
+            clearInterval(interval);
+            //Load up Ending screen
+            alert("you finsihed the game. Congrats. I have Spoken");
+        }
+    }
+
+    //set our timer////
+    function updateTime() {
+        //Make sure time isn't over and its is shownng correc time
+        timer--;
+        if (timer == 0) {
+            timer = 5;
+            counter.innerText = timer;
+            nextQuestion();
+        }
+        else {
+            counter.innerText = timer;
+        }
+    }
+
+    function allQuestions(q) {
+        console.log(q.ezQ[49]);
+        let qNum = 0;
+        for (let i = 0; i < totalQuestions; i++) {
+            //  We are going to shuffle
+            qNum = Math.floor(Math.random() * q.ezQ.length);
+            console.log(qNum);
+            triviaQ.push(q.ezQ[qNum]);
+            q.ezQ.splice(qNum, 1);
+        }
+        console.log(triviaQ);
+        counter.innertText = timer;
+    }
 }
 
 
@@ -77,15 +160,15 @@ function loadOptionsPage(info) {
     let instBtn = document.getElementById('instBtn');
 
 
-playBtn.addEventListener('click',function(e){
-    loadHTML ('gamePage.html');
-})
-menuBtn.addEventListener('click',function(e){
-    loadHTML ('menu.html');
-})
-instBtn.addEventListener('click',function (e){
-    loadHTML ('instruction.html');
-})
+    playBtn.addEventListener('click', function (e) {
+        loadHTML('gamePage.html');
+    })
+    menuBtn.addEventListener('click', function (e) {
+        loadHTML('menu.html');
+    })
+    instBtn.addEventListener('click', function (e) {
+        loadHTML('instruction.html');
+    })
 
 }
 function loadMenuPage(info) {
@@ -93,13 +176,13 @@ function loadMenuPage(info) {
     let easyBtn = document.getElementById('easyBtn');
     let medBtn = document.getElementById('medBtn');
     let hardBtn = document.getElementById('hardBtn');
-    easyBtn.addEventListener('click',function(e){
+    easyBtn.addEventListener('click', function (e) {
         loadHTML('gamePage.html');
     })
-    medBtn.addEventListener('click',function(e){
+    medBtn.addEventListener('click', function (e) {
         loadHTML('gamePage.html');
     })
-    hardBtn.addEventListener('click',function(e){
+    hardBtn.addEventListener('click', function (e) {
         loadHTML('gamePage.html');
     })
 }
@@ -108,13 +191,13 @@ function loadInstrucionPage(info) {
     let playBtn = document.getElementById('playBtn');
     let optBtn = document.getElementById('optBtn');
 
-    playBtn.addEventListener('click',function(e){
-        loadHTML ('gamePage.html');
+    playBtn.addEventListener('click', function (e) {
+        loadHTML('gamePage.html');
 
     })
 
-    optBtn.addEventListener('click',function(e){
-        loadHTML ('options.html')
+    optBtn.addEventListener('click', function (e) {
+        loadHTML('options.html')
     })
 }
 //-----------------------------------------------------------------------------------------------------------////
@@ -133,129 +216,8 @@ function loadInstrucionPage(info) {
 //     });
 // }
 //creat our JSON DATEA LOAD//
-function loadJSON(url) {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            tQuestions = JSON.parse(this.responseText).easyQ;
-            ///------------radnomizes questions------------////
-            function loadQuestion() {
-                let xmlhttp = new XMLHttpRequest();
-                let url = "easyQ.json";
-
-                if (diff == "easy") {
-                    url = "easyQ.json";
-                }
-                if (diff == "") {
-
-                }
-                xmlhttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                        let myArr = JSON.parse(this.responseText);
-                        allQuestions(myArr);
-                    }
-                }
-                xmlhttp.open("GET", url, true);
-                xmlhttp.send();
-            }
-            function allQuestions(q) {
-                console.log(q.ezQ[49]);
-                let qNum = 0;
-                for (let i = 0; i < totalQuestions; i++) {
-                    //  We are going to shuffle
-                    qNum = Math.floor(Math.random() * q.ezQ.length);
-                    console.log(qNum);
-                    triviaQ.push(q.ezQ[qNum]);
-                    q.ezQ.splice(qNum, 1);
-                }
-                console.log(tQuestions);
-                counter.innertText = timer;
-                interval = setInterval(updateTime, 1000);
-                loadQuestion();
-            }
-
-            //////////////////////randomizes questions///////////////////
-        }
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
-//loadJSON(diff1)
-///----------------------------------------------------///
-
-function loadQuestion()
-
-///Load the next quuestion//
-{
-
-    questions.innerText = tQuestions[qNum].q;
-
-    a1.innerText = tQuestions[qNum].a1;
-    a2.innerText = tQuestions[qNum].a2;
-    a3.innerText = tQuestions[qNum].a3;
-    a4.innerText = tQuestions[qNum].a4;
-
-    //for loop in buttons//
 
 
-}
-///-----------------//////
-function checkAnswer(answer) {
-    //Retrive the answer and see if its correct
-    //increment your correct number
-
-    if (answer === tQuestions[qNum].C) {
-        totalScore++;
-    }
-    else {
-        incorrect++;
-    }
-    C.innerText = '${totalScore}/${totalQuestions}';
-    timer = 5;
-    counter.innerText = timer;
-    nextQuestion();
-}
-
-///next question//
-
-function nextQuestions() {
-    //prep  to go to the next question
-    //loadQuestion
-
-    if (qNum < totalQuesitons) {
-
-        ///will runutil you hit toal questions = 20;
-
-        qNum++;
-        loadQuestion();
-    }
-    else {
-        //
-        clearInterval(interval);
-        //Load up Ending screen
-        alert("you finsihed the game. Congrats. I have Spoken");
-    }
-
-}
-
-//--------------------//------------------------------////
-//set our timer////
-function updateTime() {
-    //Make sure time isn't over and its is shownng correc time
-
-    timer--;
-    if (timer == 0) {
-        timer = 5;
-        counter.innerText = timer;
-        nextQuestion();
-    }
-    else {
-
-        counter.innerText = timer;
-    }
-
-
-}
 
 
 loadHTML('title.html');
